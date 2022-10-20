460ca7a52eb759f1dc0a94447d6c4688f1cf8f61
haofang <haofang97@gmail.com>
2022-10-20 16:09:59 +0800

    edited Employers and jobPosts Schema

---
+++ router/jobSeekers.js
@@ -0,0 +1,9 @@
+// Require
+const express = require("express");
+const router = express.Router();
+// const {} = require("../controllers/jobSeekers");
+
+// Endpoints
+
+// Export
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/db");
+const jobSeekers = require("./router/jobSeekers");
 
 // Define Variables
 const app = express();
@@ -16,56 +17,59 @@ app.use(express.urlencoded({ extended: false }));
 // Connect DB
 connectDB(process.env.MONGODB_URI);
 
-// Test Route
-app.get("/", (req, res) => res.json("Test Route is Working"));
+// // Test Route
+// app.get("/", (req, res) => res.json("Test Route is Working"));
 
-// Test Employers
-const Employers = require("./models/Employers");
-app.put("/employers", async (req, res) => {
-  try {
-    await Employers.deleteMany({});
-    await Employers.create(req.body);
-    res.json({
-      status: "okay",
-      message: "test object is created in the database",
-    });
-  } catch (err) {
-    console.log("PUT /employers", err);
-    res.status(400).json({ status: "error", message: "an error has occurred" });
-  }
-});
+// // Test Employers
+// const Employers = require("./models/Employers");
+// app.put("/employers", async (req, res) => {
+//   try {
+//     await Employers.deleteMany({});
+//     await Employers.create(req.body);
+//     res.json({
+//       status: "okay",
+//       message: "test object is created in the database",
+//     });
+//   } catch (err) {
+//     console.log("PUT /employers", err);
+//     res.status(400).json({ status: "error", message: "an error has occurred" });
+//   }
+// });
 
-// Test JobPosts
-const JobPosts = require("./models/JobPosts");
-app.put("/jobposts", async (req, res) => {
-  try {
-    await JobPosts.deleteMany({});
-    await JobPosts.create(req.body);
-    res.json({
-      status: "okay",
-      message: "test object is created in the database",
-    });
-  } catch (err) {
-    console.log("PUT /jobposts", err);
-    res.status(400).json({ status: "error", message: "an error has occurred" });
-  }
-});
+// // Test JobPosts
+// const JobPosts = require("./models/JobPosts");
+// app.put("/jobposts", async (req, res) => {
+//   try {
+//     await JobPosts.deleteMany({});
+//     await JobPosts.create(req.body);
+//     res.json({
+//       status: "okay",
+//       message: "test object is created in the database",
+//     });
+//   } catch (err) {
+//     console.log("PUT /jobposts", err);
+//     res.status(400).json({ status: "error", message: "an error has occurred" });
+//   }
+// });
 
-// Test JobSeekers
-const JobSeekers = require("./models/JobSeekers");
-app.put("/jobseekers", async (req, res) => {
-  try {
-    await JobSeekers.deleteMany({});
-    await JobSeekers.create(req.body);
-    res.json({
-      status: "okay",
-      message: "test object is created in the database",
-    });
-  } catch (err) {
-    console.log("PUT /jobseekers", err);
-    res.status(400).json({ status: "error", message: "an error has occurred" });
-  }
-});
+// // Test JobSeekers
+// const JobSeekers = require("./models/JobSeekers");
+// app.put("/jobseekers", async (req, res) => {
+//   try {
+//     await JobSeekers.deleteMany({});
+//     await JobSeekers.create(req.body);
+//     res.json({
+//       status: "okay",
+//       message: "test object is created in the database",
+//     });
+//   } catch (err) {
+//     console.log("PUT /jobseekers", err);
+//     res.status(400).json({ status: "error", message: "an error has occurred" });
+//   }
+// });
+
+// Routes
+app.use("/api/jobseekers", jobSeekers);
 
 // Listen
 app.listen(PORT);
