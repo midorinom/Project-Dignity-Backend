a358c5f9ba3a1e87289348c1523c4df7c8d3288d
haofang <haofang97@gmail.com>
2022-10-20 20:14:19 +0800

    [api] added jobSeekersCreate

--- controllers/jobSeekers.js
+++ controllers/jobSeekers.js
@@ -21,7 +21,20 @@ const jobSeekersPost = async (req, res) => {
 // ======
 const jobSeekersCreate = async (req, res) => {
   try {
-    await JobSeekers.create(req.body);
+    await JobSeekers.updateOne(
+      { _id: req.body.id },
+      {
+        about: req.body.about,
+        skills: req.body.skills,
+        abilityDifferences: req.body.abilityDifferences,
+        experience: req.body.experience,
+        education: req.body.education,
+      }
+    );
+    res.json({
+      status: "okay",
+      message: "a new job seeker profile is created",
+    });
   } catch (err) {
     console.log("PATCH /api/jobseekers/create", err);
     res.status(400).json({
--- router/jobSeekers.js
+++ router/jobSeekers.js
@@ -1,11 +1,14 @@
 // Require
 const express = require("express");
 const router = express.Router();
-const { jobSeekersPost } = require("../controllers/jobSeekers");
+const {
+  jobSeekersPost,
+  jobSeekersCreate,
+} = require("../controllers/jobSeekers");
 
 // Endpoints
 router.post("/post", jobSeekersPost);
-router.patch("/create", jobSeekersPost);
+router.patch("/create", jobSeekersCreate);
 
 // Export
 module.exports = router;
