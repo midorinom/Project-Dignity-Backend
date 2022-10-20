e563da16056e7ea8d011a53e9f08f1bead1bcb1a
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-21 00:01:09 +0800

    [api] added usersLogin and bcrypt

--- router/users.js
+++ router/users.js
@@ -1,10 +1,11 @@
 // Require
 const express = require("express");
 const router = express.Router();
-const { usersCreate } = require("../controllers/users");
+const { usersCreate, usersLogin } = require("../controllers/users");
 
 // Endpoints
 router.put("/create", usersCreate);
+router.post("/login", usersLogin);
 
 // Export
 module.exports = router;
