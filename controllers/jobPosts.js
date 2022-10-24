11513b8c4ef447e9ae400ff6507c49e267608a84
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-24 13:25:51 +0800

    removed GetAll for JobPosts

--- controllers/jobPosts.js
+++ controllers/jobPosts.js
@@ -72,99 +72,92 @@ const jobPostsGet = async (req, res) => {
     // This array will be sent as the response
     let jobPosts = [];
 
-    // -------
-    // Get All
-    // -------
-    if (Object.keys(req.body).length === 0) {
-      jobPosts = await JobPosts.find();
-    } else {
-      // ------
-      // Search
-      // ------
-      // This will be used in the .find, alongside the filters
-      let search = [];
-      const arrayOfSearchInputs = req.body.search.split(" "); // split by space
+    // ------
+    // Search
+    // ------
+    // This will be used in the .find, alongside the filters
+    let search = [];
+    const arrayOfSearchInputs = req.body.search.split(" "); // split by space
 
-      for (searchInput of arrayOfSearchInputs) {
-        const regex = new RegExp(searchInput, "i"); // the "i" option means case insensitive
-        search.push({
-          $or: [
-            { "jobPost.about.title": regex },
-            { "jobPost.about.company": regex },
-            { "jobPost.about.type": regex },
-            { "jobPost.about.desc": regex },
-            { "jobPost.about.tasks": regex },
-            { "jobPost.about.skills": regex },
-          ],
-        });
-      }
-
-      // -------
-      // Filters
-      // -------
-      // Empty objects by default. Only set to the filter syntax if the respective filters are selected as all (exist in req.body).
-      let abilityDiffFilter = {};
-      let supportFilter = {};
-      let customerFacingFilter = {};
-      let environmentFilter = [];
-
-      if (req.body.abilityDiff.length > 0) {
-        abilityDiffFilter = {
-          "jobPost.accessibility.abilityDiff": {
-            $all: req.body.abilityDiff,
-          },
-        };
-      }
-
-      if (req.body.support.length > 0) {
-        supportFilter = {
-          "jobPost.accessibility.support": {
-            $all: req.body.support,
-          },
-        };
-      }
-
-      if (req.body.customerFacing !== undefined) {
-        customerFacingFilter = {
-          "jobPost.about.customerFacing": req.body.customerFacing,
-        };
-      }
-
-      environmentFilter = [
-        {
-          "jobPost.accessibility.environment.noise": {
-            $gte: req.body.environment.minNoise,
-          },
-        },
-        {
-          "jobPost.accessibility.environment.noise": {
-            $lte: req.body.environment.maxNoise,
-          },
-        },
-        {
-          "jobPost.accessibility.environment.light": {
-            $gte: req.body.environment.minLight,
-          },
-        },
-        {
-          "jobPost.accessibility.environment.light": {
-            $lte: req.body.environment.maxLight,
-          },
-        },
-      ];
-
-      // This will be used in the .find, alongside search
-      const filters = [
-        abilityDiffFilter,
-        supportFilter,
-        customerFacingFilter,
-        ...environmentFilter,
-      ];
-
-      jobPosts = await JobPosts.find({
-        $and: [...search, ...filters],
+    for (searchInput of arrayOfSearchInputs) {
+      const regex = new RegExp(searchInput, "i"); // the "i" option means case insensitive
+      search.push({
+        $or: [
+          { "jobPost.about.title": regex },
+          { "jobPost.about.company": regex },
+          { "jobPost.about.type": regex },
+          { "jobPost.about.desc": regex },
+          { "jobPost.about.tasks": regex },
+          { "jobPost.about.skills": regex },
+        ],
       });
     }
+
+    // -------
+    // Filters
+    // -------
+    // Empty objects by default. Only set to the filter syntax if the respective filters are selected as all (exist in req.body).
+    let abilityDiffFilter = {};
+    let supportFilter = {};
+    let customerFacingFilter = {};
+    let environmentFilter = [];
+
+    if (req.body.abilityDiff.length > 0) {
+      abilityDiffFilter = {
+        "jobPost.accessibility.abilityDiff": {
+          $all: req.body.abilityDiff,
+        },
+      };
+    }
+
+    if (req.body.support.length > 0) {
+      supportFilter = {
+        "jobPost.accessibility.support": {
+          $all: req.body.support,
+        },
+      };
+    }
+
+    if (req.body.customerFacing !== undefined) {
+      customerFacingFilter = {
+        "jobPost.about.customerFacing": req.body.customerFacing,
+      };
+    }
+
+    environmentFilter = [
+      {
+        "jobPost.accessibility.environment.noise": {
+          $gte: req.body.environment.minNoise,
+        },
+      },
+      {
+        "jobPost.accessibility.environment.noise": {
+          $lte: req.body.environment.maxNoise,
+        },
+      },
+      {
+        "jobPost.accessibility.environment.light": {
+          $gte: req.body.environment.minLight,
+        },
+      },
+      {
+        "jobPost.accessibility.environment.light": {
+          $lte: req.body.environment.maxLight,
+        },
+      },
+    ];
+
+    // This will be used in the .find, alongside search
+    const filters = [
+      abilityDiffFilter,
+      supportFilter,
+      customerFacingFilter,
+      ...environmentFilter,
+    ];
+
+    jobPosts = await JobPosts.find({
+      $and: [...search, ...filters],
+    });
 
     // Return
     res.json(jobPosts);
